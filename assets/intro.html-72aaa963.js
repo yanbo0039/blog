f34a0d20718f0e5f92d93077c8e2a4f4db8f0acb
yanbo0039 <yanbo0039@yeah.net>
2023-05-20 14:42:53 +0000

    Deploying to gh-pages from @ vueJsObject/vuepress-theme-hope@f1c7f286230f8f2d93ae05f955c29168dfb03cb8 🚀

---
+++ assets/intro.html-72aaa963.js
@@ -0,0 +1,61 @@
+import{_ as i,X as n,Y as e,a0 as l}from"./framework-68dd73a2.js";const s={},d=l(`<h1 id="介绍页" tabindex="-1"><a class="header-anchor" href="#介绍页" aria-hidden="true">#</a> 介绍页</h1><blockquote><p>大家好，欢迎来到我的博客。我叫涎涎，来自中国大陆。曾做过全职的前端开发和Java开发， 我对自己的定位是一名全栈开发。我认为作为一名程序开发人员，可以通过以下几个点体现他的能力</p></blockquote><ol><li><strong>文件的阅读能力</strong></li></ol><blockquote><p>基本上参照各种技术的官网API可能解决一大半的问题，当然这需要你有很强的阅读理解能力,以及幂 次方的排列组合的大脑运算能力，是一个语文阅读理解加上数学的心算能力的综合体现，一个合格的程序 开发人员基本上每天阅读的文档应该不下于30~50篇视开发任务而定，但这并不能解决问题的全部， 这引申出来另一项目能力了 --&gt;</p></blockquote><ol start="2"><li><strong>搜索引擎能力</strong></li></ol><blockquote><p>这个能力尤为重要，你必须保证你的搜索引擎多样化，不任信单一的搜索结果横向对比才能醉有可能找到 自己想要的结果，当然这里的搜索结果包含文档跟视频，包含大陆地区以及墙外的地区。</p></blockquote><ol start="3"><li><strong>需求的调研能力</strong></li></ol><blockquote><p>这也是一种非常重要的能力，你必须充分的花大把时间去理解需求，这样会为你节省很多无用的开发时间 毕竟磨刀不误砍柴功。</p></blockquote><ol start="4"><li><strong>代码的可变以及复用能力</strong></li></ol><blockquote><p>以上的铺垫只为这一刻的编码，但同时也是醉直观的展示开发人员的能力的体现，当然每个方法力求考虑 周全，可以重复使用以及写测试案例等都是能力的体现。</p></blockquote><h3 id="面试需求" tabindex="-1"><a class="header-anchor" href="#面试需求" aria-hidden="true">#</a> 面试需求</h3><blockquote><p>Java基础扎实，精通io、多线程、集合等基础框架熟悉J2EE规范，熟悉常用的设计模式，精通B/S系统的开发和应用； 通SpringBoot、SpringMVC、SpringCloud、Mybatis、Redis等技术框架下的web开发，并对框架原理有一定了解。 具有分布式开发、消息队列开发经验，对分布式事务有一定了解，并能提供相应的解决方案，对数据库分库、分表有开发经验； 具备企业Web 核心系统开发、设计工作经验，熟悉前、后端分类API设计、熟悉微服务设计方案； 具备主流数据库的设计开发技能，如MySQL、MongoDB、elasticsearch等。 熟练SQL优化、掌握事务、锁、并发等机制、数据库优化、数据库设计； 熟悉软件工程、UML、设计模式，对系统设计有深刻的理解， 熟练掌握面向对象软件设计方法和设计工具，有良好的文档能力，具有系统架构设计能力，具有项目推进能力； 对devops、docker、服务器运维、Linux系统操作，Linux下的服务环境搭建等知识了解； 熟悉web前端开发流程,了解HTML,CSS，熟练使用前端开发框架：如VUE、Angular等； 强的业务分析、理解能力，并能够再在此基础上进行适当的归纳、整理，推进业务架构建设。 善于学习和总结，能独立分析和解决问题。有较强的责任心，良好的沟通能力， 积极主动的工作态度。具备良好的团队合作精神、执行力和战斗力；</p><p>熟悉微服务架构，熟悉Spring、 SpringBoot 、SpringCloud等开源框架； 熟悉中间件和消息机制，熟悉事务性系统建设的要求； 熟悉多线程及高性能的设计与编码及性能调优； 熟悉Linux 操作系统、MySql或者其他数据库；熟悉Docker 熟悉系统安全防护以及密码类使用； 熟悉VUE/React等前端框架； 具备良好的表达和沟通能力并具有较强的分析问题、方案整合和解决问题的能力 良好的团队合作精神，关注和乐于尝试最新的技术，从技术趋势和思路上能影响团队。</p><ol><li><p>计算机相关专业专科及以上；</p></li><li><p>职位相关工作经验：3-5年；</p></li><li><p>熟悉git、maven的常用命令；熟练运用idea工具高效编码 ； 使用工具查询日志；熟悉maven的常用插件；能够定位报警中的问题；</p></li><li><p>熟悉Java常用API；了解并能使用常用的第三方库，例如Apache的commons集合类、Guava等； 了解JDK的新特性； 熟练运用面向对象编程；</p></li><li><p>熟悉Spring、SpringBoot、SpringCloud的使用；</p></li><li><p>了解常用类库、中间件的使用和基本原理，例如：Redis的数据结构、MQ的机制等；ES(加分项)。</p></li><li><p>5年以上Java Web开发经验。</p></li><li><p>精通后端Java、JDBC、Springboot等开发。</p></li><li><p>熟悉常用设计模式。</p></li><li><p>具有独立完成模块设计及开发能力。</p></li><li><p>具有良好的文档编写习惯。</p></li><li><p>熟练掌握关系数据库基本操作。</p></li><li><p>有一定的微服务、云原生开发经验；</p></li><li><p>熟悉struts优先、熟练掌握常用shell命令优先；</p></li></ol></blockquote><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>1.负责项目相关开发工作，包括参与系统需求分析、系统设计、研发设计、问题排查修复；
+2.负责系统核心代码编码；
+3.负责研发小组的管理及任务安排；
+4.负责code review，指导小组成员完成任务；
+5.负责内部技术培训、知识传授； 
+6.有良好的表达和沟通能力，完成系统功能相关文档材料的编写，完善项目文档。
+任职要求
+1.具有2-5年的Web开发经验，有独自带领团队完成项目交付全过程的优先；
+2.熟练掌握Spring、SpringMVC、Hibernate、MyBatis等主流Java开发框架，对工作流有一定的了解，熟悉SpringBoot、SpringCloud框架的可优先考虑；
+3.具有前端开发经验，熟悉一种或多种javascript框架，如JQuery、Vue等等，了解常用的前端开发框架，如BootStrap、Layui、Element等等；
+4.熟悉MySQL、Oracle、MongoDB等主流数据库，对redis等缓存框架及MQ有一定的了解及应用；
+5.熟悉Maven、SVN、GIT等常用的项目管理工具；
+6.熟悉Linux操作系统，能在Linux环境下进行软件项目的安装与部署；
+7.思路清晰、善于思考，能够独立分析及解决问题；
+8.有项目管理经验者优先考虑；
+9.责任心强、具备良好的沟通能力和团队合作精神，能适应短期的出差，有一定的抗压能力；
+10.有政务服务项目经验者可优先考虑。
+
+
+1、良好的沟通能力（必须）
+2、3年以上软件开发经验，精通Java语言，熟悉软件开发流程，熟悉常用项目构建工具，如Maven等，熟练使用常用Linux命令以及Git等版本控制工具；
+3、 精通Spring、SpringCloud、Mybatis、MQ等主流开源框架和技术。
+4、熟悉常用数据库(MySQL、Redis等)的原理和使用，具有良好的数据库设计能力，对SQL编写和优化有丰富的经验；
+5、熟悉Web前端开发，熟悉JavaScript、HTML、CSS等常用前端技术，了解Angular、Vue等主流框架；
+6. 具备模块或子系统的架构设计能力，掌握常见的架构设计方法和模式，理解大型网站所需要用到的架构和技术。
+7. 能独立解决问题，能够负责重要业务模块的需求分析及设计实现，且对线上部署环境比较熟悉，能够独立分析和快速排查线上故障。
+具备以下能力优先考虑：
+1. 熟悉开源产品，有开源贡献.
+2. 了解分布式、延展性、可扩性架构设计，具备大流量、大访问量、高负载环境下的系统开发及优化经验；
+3. 熟悉敏捷开发流程，以及TDD等极限编程技术。
+
+
+1、有参与需求分析、系统设计、功能分析、功能开发、功能测试、性能测试和项目部署、项目维护过程的经历，了解jvm，懂性能优化;
+2、熟悉Spring、SpringMVC、SpringBoot、Restful Webservice相关的技术、Mybatis等Java主流框架,可熟练运用Java技术进行开发和维护;
+3、熟练使用Linux操作系统、Git/SVN、Tomcat、nginx等开发环境和工具;
+4. 熟悉Mysql、MongoDB、redis数据库，熟练编写SQL语句，能熟练编写相应的触发器以及存储过程，具有数据库性能优化经验;
+
+
+1、熟悉微服务，有微服务开发经验优先
+2、熟悉SSM、SpringMVC、SpringBoot、Spring、MyBatis等主流技术特性
+3、具有spring cloud,dubbo等相关框架开发经验优先；
+4、熟练使用MySQL、Oracle等数据库，并具有一定的SQL优化能力，熟悉Redis等常用NoSQL解决方案；
+5、提供住宿
+
+
+1.熟悉JAVA语言，spring  MVC  mysql数据库，
+2.web应用项目经验 ，2-4年经验
+3.具有一定的需求分析能力
+4.JAVA中级
+5.需要会使用Axure做原型（类似UED），前期要配合输出原型
+
+
+1、5年以上前端开发工作经验, 必须熟悉VUE3和typescript
+2、掌握HTML5，CSS3，JavaScript等基础技术，熟悉W3C标准及规范，具备良好的代码编写习惯；
+3、擅长Vue或者React技术栈，具有独立项目开发能力；
+4、了解前端模块化，能够编写易于维护的前端代码；
+5、有较好的ES6基础和扎实的JavaScript基础，对前后端分离、模块化有深刻了解
+6、熟悉前端工程化构架，有glup、webpack配置和优化经验优先
+7、至少掌握一门后端语言者优先；
+8、较强的沟通能力及团队合作能力。
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,13),a=[d];function v(r,c){return n(),e("div",null,a)}const o=i(s,[["render",v],["__file","intro.html.vue"]]);export{o as default};
