19db2787d0cb60ffb90ca4453ba9a913d8b8eaec
yanbo0039 <yanbo0039@yeah.net>
2023-06-20 16:38:29 +0000

    Deploying to gh-pages from @ vueJsObject/vuepress-theme-hope@8d7b1bd830d8e35947d0f9375aefe079801b2f1f 🚀

---
+++ assets/mysql13.html-442a6ea8.js
@@ -0,0 +1,52 @@
+import{_ as e,X as i,Y as d,a1 as l,a0 as n}from"./framework-68dd73a2.js";const s={},a=n(`<h1 id="mysql-常规的sql优化" tabindex="-1"><a class="header-anchor" href="#mysql-常规的sql优化" aria-hidden="true">#</a> Mysql_常规的sql优化</h1><div class="hint-container warning"><p class="hint-container-title">注意</p><p>本博文仅供学术研究和交流参考，严禁将其用于商业用途。如因违规使用产生的任何法律问题，使用者需自行负责。</p></div><h3 id="概念" tabindex="-1"><a class="header-anchor" href="#概念" aria-hidden="true">#</a> 概念</h3><ul><li>对于MySQL的SQL优化，可以考虑以下几个方面：</li></ul><ol><li><p>使用合适的索引：索引是提高查询性能的关键。根据实际的查询需求，合理创建索引，可以加快查询速度。需要注意的是，过多或不必要的索引可能会降低写操作的性能，因此需要权衡。</p></li><li><p>编写高效的SQL语句：编写优化的SQL语句可以提升查询性能。避免使用<code>SELECT *</code>查询所有字段，而是只查询所需字段。避免使用<code>SELECT DISTINCT</code>和<code>ORDER BY RAND()</code>等耗时的操作。合理使用<code>JOIN</code>查询和子查询，避免多次查询数据库。</p></li><li><p>优化表结构：合理设计表的结构，将相关的字段放在一起，避免冗余字段。选择合适的数据类型，避免过大或过小的字段长度。根据查询需求拆分表，避免过大的表影响查询性能。</p></li><li><p>避免全表扫描：使用索引和适当的查询条件，避免全表扫描。使用<code>EXPLAIN</code>命令分析查询语句的执行计划，查看是否有全表扫描或索引未命中的情况。</p></li><li><p>使用批量操作：对于需要批量插入或更新的数据，使用批量操作可以显著提高性能。例如，使用<code>INSERT INTO ... VALUES (...), (...), ...</code>语法进行批量插入。</p></li><li><p>避免频繁的连接和断开：连接和断开数据库的操作是耗时的，尽量减少频繁的连接和断开操作。可以使用连接池技术，复用数据库连接。</p></li><li><p>配置适当的缓存：对于频繁查询的数据，可以使用缓存技术，将数据缓存在内存中，减少数据库查询的次数。</p></li><li><p>定期优化数据库：定期进行数据库维护和优化操作，例如，清理无用的数据、优化表结构、重新统计索引等。</p></li></ol><blockquote><p>需要注意的是，SQL优化需要根据具体的业务场景和数据库情况进行调优， 没有一种通用的解决方案。通过不断的观察和分析数据库的性能指标， 结合具体的查询需求和数据特点，进行针对性的优化。</p></blockquote><h3 id="示例" tabindex="-1"><a class="header-anchor" href="#示例" aria-hidden="true">#</a> 示例</h3><ul><li>当涉及到MySQL的SQL优化时，以下是一些常见的示例：</li></ul><ol><li><p>使用合适的索引：</p><ul><li>示例1：为用户表的用户名字段创建索引<div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>CREATE INDEX idx_username ON user (username);
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div></li><li>示例2：为订单表的用户ID和创建时间字段创建联合索引<div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>CREATE INDEX idx_user_order ON orders (user_id, created_at);
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div></li></ul></li><li><p>编写高效的SQL语句：</p><ul><li>示例1：避免使用<code>SELECT *</code>，只查询所需字段<div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>SELECT id, name, age FROM users;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div></li><li>示例2：避免使用<code>SELECT DISTINCT</code>，使用合适的条件过滤重复数据<div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>SELECT user_id FROM orders WHERE order_status = &#39;completed&#39;;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div></li><li>示例3：避免使用<code>ORDER BY RAND()</code>，使用其他方式随机排序<div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>SELECT id, name FROM products ORDER BY RAND() LIMIT 10;  -- 需要优化
+SELECT id, name FROM products WHERE id &gt;= RAND() * (SELECT MAX(id) FROM products) LIMIT 10;  -- 优化后的随机查询
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div></li></ul></li><li><p>优化表结构：</p><ul><li>示例1：避免冗余字段，将相关字段放在一起<div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>CREATE TABLE users (
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    age INT,
+    email VARCHAR(100)
+);
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div></li><li>示例2：选择合适的数据类型，避免过大或过小的字段长度<div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>CREATE TABLE products (
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    price DECIMAL(10, 2),
+    description TEXT
+);
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div></li><li>示例3：根据查询需求拆分表，避免过大的表<div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>CREATE TABLE orders (
+    id INT PRIMARY KEY,
+    user_id INT,
+    order_date DATE,
+    ...
+);
+
+CREATE TABLE order_items (
+    order_id INT,
+    product_id INT,
+    quantity INT,
+    ...
+);
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div></li></ul></li><li><p>避免全表扫描：</p><ul><li>示例1：使用索引和适当的查询条件<div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>SELECT * FROM customers WHERE country = &#39;USA&#39;;  -- 需要索引在country字段上
+SELECT * FROM orders WHERE order_date &gt;= &#39;2022-01-01&#39;;  -- 需要索引在order_date字段上
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div></li><li>示例2：使用<code>EXPLAIN</code>命令分析查询语句的执行计划<div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>EXPLAIN SELECT * FROM products WHERE price &gt; 100;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div></li></ul></li><li><p>使用批量操作：</p><ul><li>示例1：使用<code>INSERT INTO ... VALUES (...), (...), ...</code>进行批量插入<div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>INSERT INTO users (id, name) VALUES (1, &#39;John&#39;), (2, &#39;Jane&#39;), (3, &#39;Mike&#39;);
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div></li></ul></li><li><p>避免在查询条件中使用函数：</p><ul><li>示例：避免在WHERE子句中使用函数，以充分利用索引<div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>-- 需要优化
+SELECT * FROM products WHERE YEAR(created_at) = 2022;
+
+-- 优化后
+SELECT * FROM products WHERE created_at &gt;= &#39;2022-01-01&#39; AND created_at &lt; &#39;2023-01-01&#39;;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div></li></ul></li><li><p>使用连接查询替代子查询：</p><ul><li>示例：使用JOIN语句替代子查询，减少查询次数<div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>-- 需要优化
+SELECT * FROM orders WHERE user_id IN (SELECT id FROM users WHERE country = &#39;USA&#39;);
+
+-- 优化后
+SELECT o.* FROM orders o JOIN users u ON o.user_id = u.id WHERE u.country = &#39;USA&#39;;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div></li></ul></li><li><p>避免使用SELECT *：</p><ul><li>示例：只选择需要的列，减少数据传输和处理的开销<div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>-- 需要优化
+SELECT * FROM customers;
+
+-- 优化后
+SELECT id, name, email FROM customers;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div></li></ul></li></ol><blockquote><p>这些示例展示了一些常见的SQL优化技巧，通过合适的索引、编写高效的SQL语句、 优化表结构、避免全表扫描、使用批量操作等方法，可以提升MySQL数据库的性能和响应速度。 请注意，实际的优化需要根据具体的业务场景和数据特点进行分析和调整。</p></blockquote><hr><h3 id="分割线" tabindex="-1"><a class="header-anchor" href="#分割线" aria-hidden="true">#</a> 分割线</h3><hr><div class="hint-container info"><p class="hint-container-title">相关信息</p><p><strong>以上就是我关于 <em>Mysql_关系型数据库事务及其简单示例</em> 知识点的整理与总结的全部内容，希望对你有帮助。。。。。。。</strong></p></div>`,14);function r(t,c){return i(),d("div",null,[l(" more "),a])}const v=e(s,[["render",r],["__file","mysql13.html.vue"]]);export{v as default};
